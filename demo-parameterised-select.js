4fa69e4245fd451903e3720cac86ba8bf180c914
Bukola Omojowo <bomojowo@gmail.com>
2021-07-30 13:52:48 +0100

    did parameterised select using music db

--- demo-parameterised-select.js
+++ demo-parameterised-select.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
 async function doDemo() {
-  const client = new Client({ database: 'demos' });
+  const client = new Client({ database: 'musicbase' });
   await client.connect();
 
-  const text = "select * from words where category_id = $1";
-  const values = [1];
+  const text = "select * from artists where id = $1 or id = $2";
+  const values = [1, 3];
 
   const res = await client.query(text, values);
   console.log(res.rows);
